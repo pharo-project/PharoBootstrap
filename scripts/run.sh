de4eaa090bac6b9b7e4cfc4c7928886944c9132e
guille <guillermopolito@gmail.com>
2016-07-18 15:20:02 +0200

    make run script use the correct architecture

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,7 +3,7 @@
 set -ex
 
 ./pharo Pharo.image eval "
-PBBootstrapSpur50 new
+PBBootstrapSpur{$3}50 new
 	configurationVersion: '{$1}' group: '{$2}';
 	espellBackend: EPSimulatorBackend for{$3}Bit forBootstrap;
 	bootstrap"
