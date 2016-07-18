e5f5bb918ff9ceded255aa412ac4ddf936bb9b74
guille <guillermopolito@gmail.com>
2016-07-18 16:04:21 +0200

    fixed env variable accesses

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,7 +3,7 @@
 set -ex
 
 ./pharo Pharo.image eval "
-PBBootstrapSpur{$3}50 new
-	configurationVersion: '{$1}' group: '{$2}';
-	espellBackend: EPSimulatorBackend for{$3}Bit forBootstrap;
+PBBootstrapSpur${3}50 new
+	configurationVersion: '${1}' group: '${2}';
+	espellBackend: EPSimulatorBackend for${3}Bit forBootstrap;
 	bootstrap"
