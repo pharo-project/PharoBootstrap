48383e03aa881e31633e767f8ddbbdfa7bd90959
guille <guillermopolito@gmail.com>
2016-07-18 16:22:40 +0200

    fixed bootstrap class name in run script

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,7 +3,7 @@
 set -ex
 
 ./pharo Pharo.image eval "
-PBBootstrapSpur${2}50 new
+PBBootstrapSpur50${2}bit new
 	configurationVersion: '${1}' group: '${3}';
 	espellBackend: EPSimulatorBackend for${2}Bit forBootstrap;
 	bootstrap"
