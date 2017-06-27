d9d15f7fc0ed1f2438c35da5fe81813cf0cb5221
Guille Polito <guille@gotham.local>
2017-06-27 11:31:29 +0200

    not running bootstrap anymore

--- scripts/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash 
-
-set -ex
-
-./pharo Pharo.image eval "
-PBBootstrapSpur50${2}bit new
-	configurationVersion: '${1}' group: '${3}';
-	espellBackend: EPSimulatorBackend for${2}Bit forBootstrap;
-	bootstrap"
