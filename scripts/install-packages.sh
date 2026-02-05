458c31f475c35d531765a721032493ca23a6b6af
Guille Polito <guillermopolito@gmail.com>
2026-02-05 16:45:07 +0100

    Patch metacello for compatibility with Pharo14

--- scripts/install-packages.sh
+++ scripts/install-packages.sh
@@ -8,5 +8,18 @@ Metacello new
 	repository: 'filetree://.';
 	load.
 
+BaselineOf compile: 'packageRepositoryURLForSpec: spec
+
+	^ spec repositoryDescriptions
+		  detect: [ :each | each beginsWith: ''tonel://'' ]
+		  ifNone: [
+			  spec repositoryDescriptions
+				  ifEmpty: [ '''' ]
+				  ifNotEmpty: [ :collection | collection anyOne ] ]'.
+
+MetacelloBaselineConstructor compile: 'repositoryDescriptions
+	
+	^ self project repositoryDescription'.
+
 Smalltalk snapshot: true andQuit: true.
 "
